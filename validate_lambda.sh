4f401bfc2a3a0c857ccf25f7fc56b80d3eb7ad2c
Michael Stahnke <stahnke@floxdev.com>
2024-08-19 17:31:27 -0500

    chore: move shell stuf into contrib

--- validate_lambda.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-
-# This simply pulls down the latest json file from s3 to see the date. This is
-# useful for validating the lambda function is working as expected.
-
-s3cmd ls s3://flox-data-lake/gh-flox/* |  sort | tail -1
